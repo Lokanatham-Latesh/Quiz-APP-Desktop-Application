dec92414aef57221779cb44cf331776b5ec53b92
Lokanatham-Latesh <lokanathamlatesh2003@gmail.com>
2023-04-22 22:52:17 +0530

    first commit

---
+++ src/quizapplication/Login.java
@@ -0,0 +1,76 @@
+package quizapplication;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Login extends JFrame implements ActionListener {
+	JButton next,back;
+	JTextField tfname;
+	Login()
+	{
+		getContentPane().setBackground(Color.WHITE);
+		setLayout(null);
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login2.jpg"));
+		JLabel image = new JLabel(i1);
+		image.setBounds(0,0,500,500);
+		add(image);
+		
+		JLabel heading = new JLabel("Welcome Java Quiz");
+		heading.setBounds(600,60,500,40);
+		heading.setFont(new Font("Viner Hand ITC",Font.BOLD,35));
+		heading.setForeground(Color.BLUE);
+		add(heading);
+		
+		JLabel name = new JLabel("Enter your name");
+		name.setBounds(700,120,280,20);
+		name.setFont(new Font("Times New Roman",Font.BOLD,20));
+		name.setForeground(Color.BLACK);
+		add(name);
+		
+	    tfname = new JTextField();
+		tfname.setBounds(650,160,300,25);
+		tfname.setFont(new Font("Times New Roman",Font.BOLD,20));
+		add(tfname);
+		
+	      next = new JButton("Next");
+			next.setBounds(680,220,90,30);
+			next.setBackground(new Color(30,144,254));
+			next.setForeground(Color.WHITE);
+			next.addActionListener(this);
+			add(next);
+			
+	         back = new JButton("Back");
+			back.setBounds(830,220,90,30);
+			back.setBackground(new Color(30,144,254));
+			back.setForeground(Color.WHITE);
+			back.addActionListener(this);
+			add(back);
+		
+		
+		
+		setVisible(true);
+		setSize(1200,600);
+		setLocation(100,150); 
+	}
+
+
+	public void actionPerformed(ActionEvent ae) {
+		if(ae.getSource()==next)
+		{
+			String name = tfname.getText();
+			setVisible(false);
+			new Next(name);
+		}
+		else if(ae.getSource()==back)
+		{
+			setVisible(false);
+		}
+		
+	}
+
+	
+	public static void main(String[] args) {
+		
+		new Login();
+	}
+
+}
